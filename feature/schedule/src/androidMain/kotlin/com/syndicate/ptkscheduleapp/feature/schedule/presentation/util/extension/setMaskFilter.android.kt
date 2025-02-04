e605b489b4745af1d4a1c5b0bbd98538bf2e3e0c
zheniaregbl <evgenieregbl@gmail.com>
2025-02-04 23:11:18 +0300

    chore: remove unused files

--- feature/schedule/src/androidMain/kotlin/com/syndicate/ptkscheduleapp/feature/schedule/presentation/util/extension/setMaskFilter.android.kt
+++
@@ -1,8 +0,0 @@
-package com.syndicate.ptkscheduleapp.feature.schedule.presentation.util.extension
-
-import android.graphics.BlurMaskFilter
-import androidx.compose.ui.graphics.NativePaint
-
-internal actual fun NativePaint.setMaskFilter(blurRadius: Float) {
-    this.maskFilter = BlurMaskFilter(blurRadius, BlurMaskFilter.Blur.NORMAL)
-}
--- feature/schedule/src/commonMain/kotlin/com/syndicate/ptkscheduleapp/feature/schedule/presentation/util/extension/setColor.kt
+++
@@ -1,5 +0,0 @@
-package com.syndicate.ptkscheduleapp.feature.schedule.presentation.util.extension
-
-import androidx.compose.ui.graphics.NativePaint
-
-internal expect fun NativePaint.setColor(color: Int)
