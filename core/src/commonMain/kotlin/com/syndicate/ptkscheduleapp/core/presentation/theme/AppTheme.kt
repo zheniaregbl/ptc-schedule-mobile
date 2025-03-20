41c32a6fb5d98a90b7b7f839b1234f54242537a8
zheniaregbl <evgenieregbl@gmail.com>
2025-03-20 11:41:32 +0300

    style: add animation for changing app theme mode

--- core/src/commonMain/kotlin/com/syndicate/ptkscheduleapp/core/presentation/theme/AppTheme.kt
+++ core/src/commonMain/kotlin/com/syndicate/ptkscheduleapp/core/presentation/theme/AppTheme.kt
@@ -1,9 +1,19 @@
 package com.syndicate.ptkscheduleapp.core.presentation.theme
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.EaseOut
+import androidx.compose.animation.core.tween
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.graphics.Color
+
+private val themeChangeSpec = tween<Color>(
+    durationMillis = 260,
+    easing = EaseIn
+)
 
 @Composable
 fun AppTheme(
@@ -11,13 +21,37 @@ fun AppTheme(
     content: @Composable () -> Unit
 ) {
 
-    val colorPalette = when (themeMode) {
+    val targetColorPalette = when (themeMode) {
         ThemeMode.LIGHT -> LightColorPalette
         ThemeMode.GRAY -> GrayColorPalette
         ThemeMode.DARK -> DarkColorPalette
     }
 
-    CompositionLocalProvider(LocalColorPalette provides colorPalette) {
+    val animatedColorPalette = ColorPalette(
+        backgroundColor = animateColorAsState(
+            targetValue = targetColorPalette.backgroundColor,
+            animationSpec = themeChangeSpec
+        ).value,
+        contentColor = animateColorAsState(
+            targetValue = targetColorPalette.contentColor,
+            animationSpec = themeChangeSpec
+        ).value,
+        secondaryColor = animateColorAsState(
+            targetValue = targetColorPalette.secondaryColor,
+            animationSpec = themeChangeSpec
+        ).value,
+        thirdlyColor = animateColorAsState(
+            targetValue = targetColorPalette.thirdlyColor,
+            animationSpec = themeChangeSpec
+        ).value,
+        otherColor = animateColorAsState(
+            targetValue = targetColorPalette.otherColor,
+            animationSpec = themeChangeSpec
+        ).value,
+        themeMode = targetColorPalette.themeMode,
+    )
+
+    CompositionLocalProvider(LocalColorPalette provides animatedColorPalette) {
         MaterialTheme(
             colorScheme = lightColorScheme(),
             typography = MontserratTypography(),
